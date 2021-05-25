599b2e78848ccf136f6d7ef1d0961af37905a1fe
Ali Sheikh <ali.sheikh@flexport.com>
2021-05-24 22:07:48 -0400

    f

--- hash_collision.py
+++ hash_collision.py
@@ -1,10 +1,7 @@
-
-
-from hashlib import sha256
+import string
 import random
 import hashlib
-import os
-import string
+from hashlib import sha256
 
 def hash_collision(k):
     if not isinstance(k,int):
@@ -14,19 +11,29 @@ def hash_collision(k):
         print( "Specify a positive number of bits" )
         return( b'\x00',b'\x00' )
 
-    print(k)
+    print(k) #Check to see what K number if
     #Collision finding code goes here
-    letters = string.ascii_letters + string.digits + string.punctuation
-    x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
-    y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
-    while 1:
-        hashedX = sha256(x).hexdigest()
-        hashedX = bin(int(hashedX, 16))[2:].zfill(256)
-        hashedY = sha256(y).hexdigest()
-        hashedY = bin(int(hashedY, 16))[2:].zfill(256)
-        if hashedY[256-k:256] == hashedX[256-k:256]:
+    hash_letters = string.ascii_letters + string.digits + string.punctuation
+    print(hash_letters)
+
+    x = ''.join(random.choice(hash_letters) for i in range(12)).encode('utf-8')
+
+    y = ''.join(random.choice(hash_letters) for i in range(12)).encode('utf-8')
+
+    while True:
+        #Define X and Y
+        sha256X = sha256(x).hexdigest()
+        sha256Y = sha256(y).hexdigest()
+        #Convert to bin int
+        sha256X = bin(int(sha256X, 16))[2:].zfill(256)
+        sha256Y = bin(int(sha256Y, 16))[2:].zfill(256)
+
+        if sha256Y[256-k:256] == sha256X[256-k:256]:
             break
-        x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
-        y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
-    print(x, y)
+
+        #Random X
+        x = ''.join(random.choice(hash_letters) for i in range(12)).encode('utf-8')
+        #Random Y
+        y = ''.join(random.choice(hash_letters) for i in range(12)).encode('utf-8')
+    print(x, y) #check output
     return( x, y )
