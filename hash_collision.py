4539adec9199969969256eb87b24b37bd23a928e
Ali Sheikh <ali.sheikh@flexport.com>
2021-05-24 21:53:16 -0400

    f

--- hash_collision.py
+++ hash_collision.py
@@ -1,34 +1,34 @@
 
 
-import hashlib
-import os
-from hashlib import sha256
-import random
+# import hashlib
+# import os
+# from hashlib import sha256
+# import random
 
-def hash_collision(k):
+# def hash_collision(k):
 
-    if not isinstance(k,int):
-        print( "hash_collision expects an integer" )
-        return( b'\x00',b'\x00' )
-    if k < 0:
-        print( "Specify a positive number of bits" )
-        return( b'\x00',b'\x00' )
+#     if not isinstance(k,int):
+#         print( "hash_collision expects an integer" )
+#         return( b'\x00',b'\x00' )
+#     if k < 0:
+#         print( "Specify a positive number of bits" )
+#         return( b'\x00',b'\x00' )
 
-    print(k)
-    #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
-    x = str(random.random()).encode('utf-8')
-    x_bits = bin(int(sha256(x).hexdigest(),16))
-    i = 500
-    while (True):
-        y = str(i).encode('utf-8')
-        y_bits = bin(int(sha256(y).hexdigest(),16))
-        if x_bits[-k:] == y_bits[-k:]:
-            break
-        i+=1
-    print(x, y)
-    return( x, y)
+#     print(k)
+#     #Collision finding code goes here
+#     x = b'\x00'
+#     y = b'\x00'
+#     x = str(random.random()).encode('utf-8')
+#     x_bits = bin(int(sha256(x).hexdigest(),16))
+#     i = 500
+#     while (True):
+#         y = str(i).encode('utf-8')
+#         y_bits = bin(int(sha256(y).hexdigest(),16))
+#         if x_bits[-k:] == y_bits[-k:]:
+#             break
+#         i+=1
+#     print(x, y)
+#     return( x, y)
 
 # a = int(input ("input an integer: "))
 # print (hash_collision(a))
@@ -203,3 +203,34 @@ def hash_collision(k):
 #     y = b'\x00'
 
 #     return( x, y )
+
+from hashlib import sha256
+import random
+import hashlib
+import os
+import string
+
+def hash_collision(k):
+    if not isinstance(k,int):
+        print( "hash_collision expects an integer" )
+        return( b'\x00',b'\x00' )
+    if k < 0:
+        print( "Specify a positive number of bits" )
+        return( b'\x00',b'\x00' )
+
+    print(k)
+    #Collision finding code goes here
+    letters = string.ascii_letters + string.digits + string.punctuation
+    x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    while 1:
+        hashedX = sha256(x).hexdigest()
+        hashedX = bin(int(hashedX, 16))[2:].zfill(256)
+        hashedY = sha256(y).hexdigest()
+        hashedY = bin(int(hashedY, 16))[2:].zfill(256)
+        if hashedY[256-k:256] == hashedX[256-k:256]:
+            break
+        x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+        y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    print(x, y)
+    return( x, y )
