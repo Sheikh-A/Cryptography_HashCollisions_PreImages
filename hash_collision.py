4ea71bca571302520fabb4296cc42b606a9d4324
Ali Sheikh <ali.sheikh@flexport.com>
2021-05-24 21:29:01 -0400

    new

--- hash_collision.py
+++ hash_collision.py
@@ -56,13 +56,42 @@
 # if __name__ == "__main__":
 #     main()
 
+# import hashlib
+# import os
+# from hashlib import sha256
+# import random
+
+# def hash_collision(k):
+
+#     if not isinstance(k,int):
+#         print( "hash_collision expects an integer" )
+#         return( b'\x00',b'\x00' )
+#     if k < 0:
+#         print( "Specify a positive number of bits" )
+#         return( b'\x00',b'\x00' )
+
+#     #Collision finding code goes here
+#     x = b'\x00'
+#     y = b'\x00'
+#     x = str(random.random()).encode('utf-8')
+#     x_bits = bin(int(sha256(x).hexdigest(),16))
+#     i = 500
+#     while (True):
+#         y = str(i).encode('utf-8')
+#         y_bits = bin(int(sha256(y).hexdigest(),16))
+#         if x_bits[-k:] == y_bits[-k:]:
+#             break
+#         i+=1
+#     return( x, y)
+
+# a = int(input ("input an integer: "))
+# print (hash_collision(a))
+
 import hashlib
 import os
-from hashlib import sha256
-import random
+import math
 
 def hash_collision(k):
-
     if not isinstance(k,int):
         print( "hash_collision expects an integer" )
         return( b'\x00',b'\x00' )
@@ -71,18 +100,12 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
 
     #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
-    x = str(random.random()).encode('utf-8')
-    x_bits = bin(int(sha256(x).hexdigest(),16))
-    i = 500
-    while (True):
-        y = str(i).encode('utf-8')
-        y_bits = bin(int(sha256(y).hexdigest(),16))
-        if x_bits[-k:] == y_bits[-k:]:
-            break
-        i+=1
-    return( x, y)
+    while True:
+        size = int(math.log(pow(2, k), 255)) + 1
+        x = os.urandom(size)
+        y = os.urandom(size)
 
-# a = int(input ("input an integer: "))
-# print (hash_collision(a))
+        bi_hash1 = bin(int(hashlib.sha256(x).hexdigest(), 16))
+        bi_hash2 = bin(int(hashlib.sha256(y).hexdigest(), 16))
+        if bi_hash1[-k:] == bi_hash2[-k:]:
+            return (x, y)
