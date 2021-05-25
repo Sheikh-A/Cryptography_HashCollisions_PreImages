c1fa6326e7228b721a0e2887da1348c51817690a
Ali Sheikh <ali.sheikh@flexport.com>
2021-05-24 21:47:24 -0400

    f

--- hash_collision.py
+++ hash_collision.py
@@ -31,32 +31,9 @@
 # a = int(input ("input an integer: "))
 # print (hash_collision(a))
 
-# import hashlib
-# import os
-# import math
-
-# def hash_collision(k):
-#     if not isinstance(k,int):
-#         print( "hash_collision expects an integer" )
-#         return( b'\x00',b'\x00' )
-#     if k < 0:
-#         print( "Specify a positive number of bits" )
-#         return( b'\x00',b'\x00' )
-
-#     #Collision finding code goes here
-#     while True:
-#         size = int(math.log(pow(2, k), 255)) + 1
-#         x = os.urandom(size)
-#         y = os.urandom(size)
-
-#         bi_hash1 = bin(int(hashlib.sha256(x).hexdigest(), 16))
-#         bi_hash2 = bin(int(hashlib.sha256(y).hexdigest(), 16))
-#         if bi_hash1[-k:] == bi_hash2[-k:]:
-#             return (x, y)
-
 import hashlib
 import os
-from random import random
+import math
 
 def hash_collision(k):
     if not isinstance(k,int):
@@ -65,23 +42,20 @@ def hash_collision(k):
     if k < 0:
         print( "Specify a positive number of bits" )
         return( b'\x00',b'\x00' )
-
-    #Collision finding code goes here
-    x = str(random()).encode('utf-8')
-    y = str(random()).encode('utf-8')
     print(k)
+    #Collision finding code goes here
+    while True:
+        size = int(math.log(pow(2, k), 255)) + 1
+        x = os.urandom(size)
+        y = os.urandom(size)
 
-    x_hash_binary = bin(int(hashlib.sha256(x).hexdigest(), 16))
-    y_hash_binary = bin(int(hashlib.sha256(y).hexdigest(), 16))
+        bi_hash1 = bin(int(hashlib.sha256(x).hexdigest(), 16))
+        bi_hash2 = bin(int(hashlib.sha256(y).hexdigest(), 16))
+        if bi_hash1[-k:] == bi_hash2[-k:]:
+            print(x, y)
+            return (x, y)
 
-    while y_hash_binary.endswith(x_hash_binary[len(x_hash_binary)-k:]) == False:
-        x = str(random()).encode('utf-8')
-        y = str(random()).encode('utf-8')
 
-        x_hash_binary = bin(int(hashlib.sha256(x).hexdigest(), 16))
-        y_hash_binary = bin(int(hashlib.sha256(y).hexdigest(), 16))
-    print(x, y)
-    return( x, y )
 
 # /usr/bin/env python
 # coding: utf-8
